ac05ece3361d01fd9abb840d7935be1621f3a12e
Franco Rodriguez <52495065+francor2502@users.noreply.github.com>
2019-07-04 12:34:08 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,15 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var numero1;
+    var numero2;
+    var resultado;
+    numero1 = parseInt(document.getElementById("numeroDividendo").value);
+  
+    numero2 = parseInt(document.getElementById("numeroDivisor").value);
+  
+    resultado = numero1 % numero2;
+  
+    alert("El resto es " + resultado);
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var aumento;
+    var resultado;
 
-	
+    sueldo = parseFloat(document.getElementById("sueldo").value);
+
+    aumento = sueldo * 0.1;
+
+    resultado = sueldo + aumento;
+
+    document.getElementById("resultado").value = resultado;
+    
 }
