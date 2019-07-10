bf55a38ed3376c805afb98de7350c10f348c61cc
Franco Rodriguez <52495065+francor2502@users.noreply.github.com>
2019-07-10 12:20:16 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -3,6 +3,25 @@ function mostrar()
 //tomo la edad  
 
 
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if (edad >= 18) {
+    alert("Usted es un adulto");
+   
+} else {
+      
+    if (edad >=13 && edad <=17 ) {
+        alert("Usted es un adolescente");
+  
+         } else{
+        alert("Usted es un niño");
+         }
+}
+
+}
 
 
-}//FIN DE LA FUNCIÓN
+
+//FIN DE LA FUNCIÓN
