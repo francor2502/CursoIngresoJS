d5e2972b0f5d8e28727c61a8ef21540771040b59
Franco Rodriguez <52495065+francor2502@users.noreply.github.com>
2019-07-18 12:16:18 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,19 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
+	var numero;
+	
+	do {
+		numero = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero)){
+			alert("Eso no es un numero");
+			numero = parseInt(prompt("Ingrese un numero"));		
+		}
+		acumulador = acumulador + numero;
+		contador++;
+		respuesta = prompt("Quieres ingresar otro numero?");
+	
+	} while (respuesta == "si");
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,26 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var respuesta='s';
+
+	do {
+		numero = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero)){
+			alert("Eso no es un numero");
+			numero = parseInt(prompt("Ingrese un numero"));
+			contador++;
+			respuesta = prompt("Quiere ingresar otro numero?");
+		}
+	} while (respuesta == 's');
+
+	if (numero < 0) {
+		
+	} else {
+		
+	}
+
 	
-	var respuesta='si';
-
-
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
